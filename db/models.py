2137d97a92bdb5afe29ef87b4b77b9225e074ff3
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2025-12-05 20:52:57 +0100

    fixed pytest error

--- db/models.py
+++ db/models.py
@@ -31,7 +31,7 @@ class Player(models.Model):
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     guild = (
         models.ForeignKey(
-            Guild, on_delete=models.SET_DEFAULT, default="No Guild", null=True
+            Guild, on_delete=models.SET_NULL, null=True
         )
     )
     created_at = models.DateTimeField(auto_now_add=True)
