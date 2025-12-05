c2aa7da419f9dd3ba4b839d43bdc5b604f8a62c1
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2025-12-05 20:50:55 +0100

    first one yay

--- db/models.py
+++ db/models.py
@@ -1,1 +1,37 @@
 from django.db import models
+
+
+class Race(models.Model):
+    races = [
+        ("elf", "Elegant forest people"),
+        ("dwarf", "Dwellers of the underground"),
+        ("human", "Most populated race yet the most simple"),
+        ("ork", "Mightiest fighters")
+    ]
+
+    name = models.CharField(max_length=255, choices=races, unique=True)
+    description = models.TextField(blank=True, default="")
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True, default="")
+
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = (
+        models.ForeignKey(
+            Guild, on_delete=models.SET_DEFAULT, default="No Guild", null=True
+        )
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -1,10 +1,87 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
+def create_race(players: dict) -> list:
+    race_instances = []
+    for player in players:
+        race, created = Race.objects.get_or_create(
+            name=players.get(player).get("race")["name"],
+            defaults={
+                "description": players.get(player).get("race")["description"]
+            }
+        )
+        race_instances.append(race)
+    return race_instances
+
+
+def create_skills(players: dict, races: list) -> None:
+    for player in players:
+        for name_bonus in players.get(player).get("race").get("skills"):
+            race_of_this_player = players.get(player).get("race")["name"]
+            for race in races:
+                if race.name == race_of_this_player:
+                    race_obj = race
+            skill, created = Skill.objects.get_or_create(
+                name=name_bonus["name"],
+                defaults={
+                    "bonus": name_bonus["bonus"],
+                    "race": race_obj
+                }
+            )
+
+
+def create_guild(players: dict) -> list:
+    guild_instances = []
+    for player in players:
+        if players.get(player).get("guild"):
+            guild, created = Guild.objects.get_or_create(
+                name=players.get(player).get("guild")["name"],
+                defaults={
+                    "description": (
+                        players.get(player).get("guild")["description"]
+                    )
+                }
+            )
+        guild_instances.append(guild)
+    return guild_instances
+
+
+def create_player(players: dict, races: list, guilds: list) -> None:
+    for player in players:
+        race_of_this_player = players.get(player).get("race")["name"]
+        for race in races:
+            if race.name == race_of_this_player:
+                race_obj = race
+        if players.get(player).get("guild"):
+            guild_of_this_player = players.get(player).get("guild")["name"]
+            for guild in guilds:
+                if guild.name == guild_of_this_player:
+                    guild_obj = guild
+        else:
+            guild_obj = None
+        name, created = Player.objects.get_or_create(
+            nickname=player,
+            defaults={
+                "email": players.get(player)["email"],
+                "bio": players.get(player)["bio"],
+                "race": race_obj,
+                "guild": guild_obj
+            }
+        )
+
+
 def main() -> None:
-    pass
+
+    with open("players.json") as f:
+        players_file = json.load(f)
+
+    races = create_race(players_file)
+    create_skills(players_file, races)
+    guilds = create_guild(players_file)
+    create_player(players_file, races, guilds)
 
 
 if __name__ == "__main__":
