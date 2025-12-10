ff7ba89172f11e5e1d041b0ab7a9bc90fe5ccaf6
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2025-12-10 17:23:18 +0100

    reworked the logic, i hope it's better, if not please give me some suggestions

--- main.py
+++ main.py
@@ -6,87 +6,99 @@ import json
 from db.models import Race, Skill, Player, Guild
 
 
-def find_the_dict(initial_dict: dict, lookup_name: str) -> dict:
-    result_dict = {}
-    for item in initial_dict:
-        if initial_dict.get(item).get(lookup_name):
-            result_dict[item] = initial_dict.get(item).get(lookup_name)
-    return result_dict
+def get_dict(
+        initial_dict: dict,
+        field_to_look_for: str
+) -> dict:
+    return {
+        name: info.get(field_to_look_for)
+        for name, info in initial_dict.items()
+    }
 
 
-def get_or_create_race_guild(
-        players: dict,
-        key_to_find_the_dict: str,
+def get_fk(
+        info: dict,
+        instance_list: list,
+        player: str
+) -> Model|None:
+    if info.get(player):
+        fk_name = info.get(player).get("name")
+        for instance in instance_list:
+            if instance.name == fk_name:
+                return instance
+        return None
+    else:
+        return None
+
+
+def get_or_create(
+        dict_to_table: dict,
         model: Model,
-        description: str = "description"
+        info_races: dict = None,
+        instance_list: list = None
 ) -> list:
-    result_list = []
-    our_dict = find_the_dict(players, key_to_find_the_dict)
-    for player in our_dict:
-        if our_dict.get(player) is not None:
-            instance, created = model.objects.get_or_create(
-                name=our_dict.get(player).get("name"),
-                defaults={
-                    description: our_dict.get(player).get(description)
+    instances = []
+    for player, info in dict_to_table.items():
+        if info is None:
+            continue
+        if isinstance(info, dict):
+            info = [info]
+        for item in info:
+            defaults = {}
+            if not instance_list:
+                defaults["description"] = item.get("description")
+            else:
+                defaults = {
+                    "bonus": item.get("bonus"),
+                    "race": get_fk(info_races, instance_list, player)
                 }
+            instance, created = model.objects.get_or_create(
+                name=item["name"],
+                defaults=defaults
             )
-            result_list.append(instance)
-    return result_list
+            instances.append(instance)
+    return instances
 
 
-def create_skill(players: dict, races: list) -> None:
-    race_dict = find_the_dict(players, "race")
-    skill_dict = find_the_dict(race_dict, "skills")
-    for player, skills in skill_dict.items():
-        race_name = race_dict[player].get("name")
-        race_obj = next((r for r in races if r.name == race_name), None)
-        if not race_obj:
-            raise Exception("Race not found")
-        if skills:
-            for skill in skills:
-                instance, created = Skill.objects.get_or_create(
-                    name=skill["name"],
-                    defaults={
-                        "bonus": skill["bonus"],
-                        "race": race_obj
-                    }
-                )
-
-
-def create_player(players: dict, races: list, guilds: list) -> None:
-    for player in players:
-        race_obj = None
-        guild_obj = None
-        race_of_this_player = players.get(player).get("race").get("name")
-        for race in races:
-            if race.name == race_of_this_player:
-                race_obj = race
-        if race_obj is None:
-            raise Exception("Race not found")
-        if players.get(player).get("guild"):
-            guild_of_this_player = players.get(player).get("guild").get("name")
-            for guild in guilds:
-                if guild.name == guild_of_this_player:
-                    guild_obj = guild
-        name, created = Player.objects.get_or_create(
+def create_player(
+        players: dict,
+        race_info: dict,
+        races_inst: list,
+        guild_info: dict,
+        guild_inst: list
+) -> None:
+    for player, info in players.items():
+        instance, created = Player.objects.get_or_create(
             nickname=player,
             defaults={
-                "email": players.get(player).get("email"),
-                "bio": players.get(player).get("bio"),
-                "race": race_obj,
-                "guild": guild_obj
+                "email": info.get("email"),
+                "bio": info.get("bio"),
+                "race": get_fk(
+                    race_info, races_inst, player
+                ),
+                "guild": get_fk(
+                    guild_info, guild_inst, player
+                )
             }
         )
-
+    return
 
 def main() -> None:
+    Player.objects.all().delete()
+    Skill.objects.all().delete()
+    Guild.objects.all().delete()
+    Race.objects.all().delete()
+
     with open("players.json") as f:
         players_file = json.load(f)
 
-    races = get_or_create_race_guild(players_file, "race", Race)
-    guilds = get_or_create_race_guild(players_file, "guild", Guild)
-    create_skill(players_file, races)
-    create_player(players_file, races, guilds)
+    races = get_dict(players_file, "race")
+    guilds = get_dict(players_file, "guild")
+    skills = get_dict(races, "skills")
+    races_inst = get_or_create(races, Race)
+    guild_inst = get_or_create(guilds, Guild)
+    get_or_create(skills, Skill, races, races_inst)
+    create_player(players_file, races, races_inst, guilds, guild_inst)
 
 
 if __name__ == "__main__":
